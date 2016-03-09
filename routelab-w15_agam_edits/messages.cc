8f66b8eac7037807f67b0229776566d8bbfb35b8
aiqi <liuaiqi1997@hotmail.com>
2016-03-08 20:29:43 -0600

    newer version

---
+++ routelab-w15_agam_edits/messages.cc
@@ -0,0 +1,51 @@
+#include "messages.h"
+
+
+#if defined(GENERIC)
+ostream &RoutingMessage::Print(ostream &os) const
+{
+  os << "RoutingMessage()";
+  return os;
+}
+#endif
+
+
+#if defined(LINKSTATE)
+
+ostream &RoutingMessage::Print(ostream &os) const
+{
+  return os;
+}
+
+RoutingMessage::RoutingMessage()
+{}
+
+
+RoutingMessage::RoutingMessage(const RoutingMessage &rhs)
+{}
+
+#endif
+
+
+#if defined(DISTANCEVECTOR)
+
+ostream &RoutingMessage::Print(ostream &os) const
+{
+  os << "RoutingMessage(src=" << src << ", dest=" << dest << ", cost=" << cost << ")";
+  return os;
+}
+
+RoutingMessage::RoutingMessage()
+{}
+
+
+RoutingMessage::RoutingMessage(const RoutingMessage &rhs) :
+  src(rhs.src), dest(rhs.dest), cost(rhs.cost)
+{}
+
+RoutingMessage::RoutingMessage(Node s, Node d, double c) :
+  src(s), dest(d), cost(c)
+{}
+
+#endif
+
---
+++ routelab-w15_agam_edits/table.cc
@@ -0,0 +1,114 @@
+#include "table.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#if defined(GENERIC)
+ostream & Table::Print(ostream &os) const
+{
+  // WRITE THIS
+  os << "Table()";
+  return os;
+}
+#endif
+
+#if defined(LINKSTATE)
+
+Table::Table(unsigned size){
+preds.assign(size+1, size +1);
+costs.assign(size+1, 100000000.0);
+visited.assign(size+1, false);
+}
+vector<double> Table::getCosts() {
+    return costs;
+}
+
+vector<unsigned> Table::getPreds() const {
+    return preds;
+}
+vector<bool> Table::getVisited() {
+    return visited;
+}
+
+void Table::setCosts(vector<double> updatedCosts){
+    costs = updatedCosts;
+}
+
+void Table::setPreds(vector<unsigned> updatedPreds){
+    preds = updatedPreds;
+}
+void Table::setVisited(vector<bool> updatedPredsVisited){
+    visited  = updatedPredsVisited;
+}
+
+ostream & Table::Print(ostream &os) const
+{
+  for(unsigned i = 1;i<preds.size(); i++)
+      os << "Node: "<< i << "Predecessor: " << preds[i] << "Cost: " << costs[i] << endl;
+  return os;
+}
+#endif
+
+#if defined(DISTANCEVECTOR)
+RoutingPath::RoutingPath(unsigned dest, unsigned next, double c) :
+	destination(dest), next_node(next), cost(c)
+{}
+
+ostream & RoutingPath::Print(ostream &os) const
+{
+	os << "RoutingPath(dest=" << destination << ", next=" << next_node << ", cost=" << cost << ")";
+	return os;
+}
+
+deque<RoutingPath> Table::GetRoutingPaths() {
+    return contents;
+}
+
+deque<RoutingPath>::iterator Table::GetDestinationRoutingPath(unsigned dest) {
+    for(deque<RoutingPath>::iterator routing_path = contents.begin(); routing_path != contents.end(); routing_path++){
+      if(routing_path->destination == dest){
+        return routing_path;
+      } 
+    }
+    return contents.end();
+}
+
+RoutingPath* Table::GetRoutingPath(unsigned dest) {
+  deque<RoutingPath>::iterator routing_path = GetDestinationRoutingPath(dest);
+  if (routing_path != contents.end()) 
+  {
+    RoutingPath* res = new RoutingPath(routing_path->destination, routing_path->next_node, routing_path->cost);
+    return res;
+  } 
+  else 
+  {
+    return NULL;
+  }
+}
+
+void Table::EditRoutingPath(unsigned dest, RoutingPath new_routing_path) {
+    deque<RoutingPath>::iterator routing_path = GetDestinationRoutingPath(dest);
+    if (routing_path != contents.end()) {
+      routing_path->destination = new_routing_path.destination;
+      routing_path->next_node = new_routing_path.next_node;
+      routing_path->cost = new_routing_path.cost;
+    }
+    else {
+      contents.push_back(new_routing_path);
+    }
+}
+
+ostream & Table::Print(ostream &os) const
+{
+  os << "Table(routing paths={";
+  for (deque<RoutingPath>::const_iterator routing_path = contents.begin(); routing_path != contents.end(); routing_path++) { 
+    os << *routing_path << ", ";
+  }
+  os << "})";
+  return os;
+}
+
+#endif
